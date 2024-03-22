5fb5288ee38833dd0de3c4a06baaa82f0831f1bb
BiazPrasastha <biazprasastha10@gmail.com>
2024-03-22 20:59:58 +0700

    Fix news search

--- app/Livewire/News/Index.php
+++ app/Livewire/News/Index.php
@@ -40,8 +40,10 @@ class Index extends Component
         $arr =  explode(' to ', $this->range);
 
         $this->news = News::when($this->query != null, function ($query) {
-            $query->where('title', 'like', "%$this->query%")
-                ->orWhere('content', 'like', "%$this->query%");
+            $query->where(function ($q) {
+                $q->where('title', 'like', "%$this->query%")
+                    ->orWhere('content', 'like', "%$this->query%");
+            });
         })->when($this->range != null, function ($query) use ($is_range, $arr) {
             $startDate = Carbon::createFromFormat('Y-m-d', $arr[0])->startOfDay();
             $endDate = Carbon::createFromFormat('Y-m-d', ($is_range ? $arr[1] : $arr[0]))->endOfDay();
